16e5baec35009456f72ea9227e9f3ff85227d01c
Emmanuel <emmanuelmartinez122623@gmail.com>
2022-11-14 12:32:09 -0700

    Add secrets

--- api-Flare/Controllers/AuthController.cs
+++ api-Flare/Controllers/AuthController.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using Models;
-using NuGet.Common;
 using System.Configuration;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
