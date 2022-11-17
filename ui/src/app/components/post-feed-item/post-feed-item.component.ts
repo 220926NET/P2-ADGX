ad7f6588e5a3ee6b8f702498c5cfcb5e07ce69e5
Gregory Sabado <gregory793@revature.net>
2022-11-16 20:31:35 -0800

    adding buttons to profile posts

--- ui/src/app/components/post-feed-item/post-feed-item.component.ts
+++ ui/src/app/components/post-feed-item/post-feed-item.component.ts
@@ -1,30 +1,35 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { Post } from 'src/app/Models/Post';
-import { CurrentUserService } from 'src/app/services/current-user/current-user.service';
-import { PostService } from 'src/app/services/post.service';
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
+import { Post } from "src/app/Models/Post";
+import { CurrentUserService } from "src/app/services/current-user/current-user.service";
+import { PostService } from "src/app/services/post.service";
 
 @Component({
-  selector: 'app-post-feed-item',
-  templateUrl: './post-feed-item.component.html',
-  styleUrls: ['./post-feed-item.component.css']
+  selector: "app-post-feed-item",
+  templateUrl: "./post-feed-item.component.html",
+  styleUrls: ["./post-feed-item.component.css"],
 })
 export class PostFeedItemComponent implements OnInit {
-  @Input() post:Post = {} as Post;
+  @Input() post: Post = {} as Post;
   @Output() deletePost = new EventEmitter<number>();
 
-  isMyPost:boolean = false;
-  showComments:boolean = false;
+  isMyPost: boolean = false;
+  showComments: boolean = false;
 
-  constructor(private currentUserService:CurrentUserService, private postService: PostService) { }
+  constructor(
+    private currentUserService: CurrentUserService,
+    private postService: PostService
+  ) {}
 
   ngOnInit(): void {
     this.isMyPost = this.currentUserService.getUserId() == this.post.userID;
   }
 
-  DeletePost(postId: number, userId: number) {
-    if (this.isMyPost){
+  DeletePost(postId: number) {
+    if (this.isMyPost) {
       this.deletePost.emit(postId);
-      this.postService.deletePost(postId, userId).subscribe((res) => {console.log(res);});
+      this.postService.deletePost(postId).subscribe((res) => {
+        console.log(res);
+      });
     }
   }
 }
